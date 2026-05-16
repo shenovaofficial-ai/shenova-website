4c4009cbd05d3482a5b4e0ed66a472f7715825ec
Krish Doshi <krishdoshi2415@gmail.com>
2026-05-16 07:29:22 +0530

    live backend connected

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,2 @@
-window.API = "http://10.159.224.149:5000/api";
-window.API_BASE = "http://10.159.224.149:5000";
+window.API = "https://shenova-website-production.up.railway.app/api";
+window.API_BASE = "https://shenova-website-production.up.railway.app";
