395ad8bfa0f35abb0a4478a943a6cdd35123f68e
Krish Doshi <krishdoshi2415@gmail.com>
2026-06-25 21:14:10 +0530

    push

--- backend/config/imagekit.js
+++ backend/config/imagekit.js
@@ -1,9 +1,10 @@
-const ImageKit = require("@imagekit/nodejs");
+const ImageKit = require("imagekit");
 
 const imagekit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
 module.exports = imagekit;
+console.log(require("imagekit/package.json").version);
